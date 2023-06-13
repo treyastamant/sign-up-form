8ddb4ddb50147ad26545df5b66481231ccde77dc
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-06-12 21:42:09 -0600

    Basic layout of form

---
+++ script.js
@@ -0,0 +1,9 @@
+const form = document.querySelector("form");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm-password");
+const passwordError = document.querySelector("span.error");
+
+console.log(password);
+console.log(confirmPassword);
+
+
